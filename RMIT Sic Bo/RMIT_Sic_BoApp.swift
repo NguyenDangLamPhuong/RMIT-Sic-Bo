5df2e079355d86f3b104d695fb690cc7f0c322be
NguyenDangLamPhuong <67723724+NguyenDangLamPhuong@users.noreply.github.com>
2022-08-24 11:22:00 +0700

    Initial Commit

---
+++ RMIT Sic Bo/RMIT_Sic_BoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMIT_Sic_BoApp.swift
+//  RMIT Sic Bo
+//
+//  Created by Mac on 24/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct RMIT_Sic_BoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
